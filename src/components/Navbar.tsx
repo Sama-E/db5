4b532294a2bf192863d0e4f613d5ed3d8b2df34f
Sama-E <112719147+Sama-E@users.noreply.github.com>
2024-08-19 16:29:32 -0400

    mobilenav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,33 @@
 import Link from 'next/link'
 import React from 'react'
 import MobileMenu from './MobileMenu'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
       {/* LEFT */}
       <div className="">
-        <Link href='/' className="font-bold text-xl text-blue-500">DB5</Link>
+        <Link href='/' className="font-bold text-xl text-blue-600">DB5</Link>
       </div>
       {/* CENTER */}
-      <div className="hidden"> </div>
+      <div className="hidden md:flex">
+        {/* LINKS */}
+        <div className="">
+          <Link href="/" className="flex gap-2">
+            <Image src="/home.png" alt="Homepage" width={16} height={16}/>
+            <span>Homepage</span>
+          </Link>
+          <Link href="/" className="flex gap-2">
+            <Image src="/home.png" alt="Homepage" width={16} height={16}/>
+            <span>Homepage</span>
+          </Link>
+          <Link href="/" className="flex gap-2">
+            <Image src="/home.png" alt="Homepage" width={16} height={16}/>
+            <span>Homepage</span>
+          </Link>
+        </div>
+      </div>
       {/* RIGHT */}
       <div className="">
         <MobileMenu />
